1fcadecf22d99465948e3a218536d5425641f2f9
Kt.codes <tarajkristian@gmail.com>
2022-05-22 20:27:42 +0200

    Deployed

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ import Banner from './components/Banner';
 import ApiHooks from "./components/ApiHooks";
 
 
-  //   const apiUrl = 'https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/csgo/standard/search?platform=steam&query=76561198008049283';
-  //   fetch(apiUrl,{
-  //     method: 'GET',
-  //     headers: {
-  //       'TRN-Api-Key': '5330e1b3-5f7e-4c36-9ea7-58dc0ff53630',
-  //       'Accept': 'application/json',
-  //       'Accept-Encoding': 'gzip'
-  //     },
-
-
-
   const App = () => {
     return (
       <>
--- src/components/ApiHooks.js
+++ src/components/ApiHooks.js
@@ -64,7 +64,7 @@ const ApiHooks = () => {
           console.log(error.response.data);
           console.log(error.response.status);
           console.log(error.response.headers);
-          setDataAvailable("User does not exist or not authorized to access data");
+          setDataAvailable("User does not exist or there was an issue authenticating");
 
         }
       });
@@ -73,99 +73,100 @@ const ApiHooks = () => {
 
 
 
-//Used to store the user input from the search bar triggered the 'onChange' event
-const [title, setTitle] = useState('');
+  //Used to store the user input from the search bar triggered the 'onChange' event
+  const [title, setTitle] = useState('');
 
-//Function triggered by the search button that calls the API function
-function showResults() {
-  searchUser(title);
-}
+  //Function triggered by the search button that calls the API function
+  function showResults() {
+    searchUser(title);
+  }
 
-function displayStats() {
-  const result = Object.values(profileData);
-  if(result.length == 0) {
-    return(
-      <div>WAIT</div>
-    )
-  }else {
-    return (
+  //This function uses the Profile Data that got fetched and
+  //if the array is not empty it will display the design
+  function displayStats() {
+    const result = Object.values(profileData);
+    if(result.length == 0) {
+      return(
+        <div>Error</div>
+      )
+    }else {
+      return (
 
-      <div>
-        <div className="wrapper">
-  <div className="profile-card js-profile-card">
-    <div className="profile-card__img">
-      <img src={result[0].avatarUrl} alt="profile card"/>
-    </div>
+    <div>
+      <div className="wrapper">
+        <div className="profile-card js-profile-card">
+          <div className="profile-card__img">
+            <img src={result[0].avatarUrl} alt="profile card"/>
+          </div>
 
-    <div className="profile-card__cnt js-profile-cnt">
-      <div className="profile-card__name">{result[0].platformUserHandle}</div>
-      <div className="profile-card__txt">CSGO Player from <strong>{result[0].platformSlug}</strong></div>
-      <div className="profile-card-loc">
-      <span className="profile-card-loc__txt">
-      {result[3][0].stats.timePlayed.displayValue} played
-        </span>
-      </div>
+          <div className="profile-card__cnt js-profile-cnt">
+          <div className="profile-card__name">{result[0].platformUserHandle}</div>
+          <div className="profile-card__txt">CSGO Player from <strong>{result[0].platformSlug}</strong></div>
+          <div className="profile-card-loc">
+          <span className="profile-card-loc__txt">
+          {result[3][0].stats.timePlayed.displayValue} played
+          </span>
+          </div>
 
-      <div className="profile-card-inf">
-        <div className="profile-card-inf__item">
+          <div className="profile-card-inf">
+          <div className="profile-card-inf__item">
           <div className="profile-card-inf__title">{result[3][0].stats.kills.displayValue}</div>
           <div className="profile-card-inf__txt">Kills</div>
-        </div>
+          </div>
 
-        <div className="profile-card-inf__item">
+          <div className="profile-card-inf__item">
           <div className="profile-card-inf__title">{result[3][0].stats.score.displayValue}</div>
           <div className="profile-card-inf__txt">Score</div>
-        </div>
+          </div>
 
-        <div className="profile-card-inf__item">
+          <div className="profile-card-inf__item">
           <div className="profile-card-inf__title">{result[3][0].stats.deaths.displayValue}</div>
           <div className="profile-card-inf__txt">Deaths</div>
-        </div>
+          </div>
 
-        <div className="profile-card-inf__item">
+          <div className="profile-card-inf__item">
           <div className="profile-card-inf__title">{result[3][0].stats.headshots.displayValue}</div>
           <div className="profile-card-inf__txt">Headshots</div>
-        </div>
+          </div>
 
-        <div className="profile-card-inf__item">
+          <div className="profile-card-inf__item">
           <div className="profile-card-inf__title">{result[3][0].stats.matchesPlayed.displayValue}</div>
           <div className="profile-card-inf__txt">Matches</div>
+          </div>
+          </div>
+          </div>
         </div>
       </div>
-</div></div></div>
-        </div>
+    </div>
 
-  );
+    );
+
+    }
 
   }
 
-}
-
-
-return(
-
-  <>
-        <div className="form-search ">
-          <input onChange={event => setTitle(event.target.value)} type="text" placeholder="Insert Steam Username/Id" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-          <button onClick={event => showResults()}  type="button" id="button-addon2">Search</button>
-        </div>
-
-  <div><h1 className='text-center text-white mt-5'>{isDataAvailable}</h1></div>
-
-  {userData.map((data,index) => (
+  return(
 
     <>
-    <div key={index.id} className='container text-center text-white mt-2'>
-      {console.log(data.platformUserHandle) }
+    <div className="form-search ">
+      {/* The onChange event watches and stores the input field */}
+      <input onChange={event => setTitle(event.target.value)} type="text" placeholder="Insert Steam Username/Id" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+      {/* Button that triggers the showResults function */}
+      <button onClick={event => showResults()}  type="button" id="button-addon2">Search</button>
     </div>
-    {displayStats()}
+    <div><h1 className='text-center text-white mt-5'>{isDataAvailable}</h1></div>
+
+    {userData.map((data,index) => (
+
+      <>
+      {displayStats()}
+      </>
+    ))}
+
+
+
     </>
-   ))}
-
-
-
-  </>)
-
+  )
 }
 
 
